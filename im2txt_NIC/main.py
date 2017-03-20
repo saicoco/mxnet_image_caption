20490a6ad3712b74eef3fbe271ee9d775b903075
saicoco <gjj0816@gmail.com>
2017-03-20 16:01:14 +0800

    main

--- im2txt_NIC/main.py
+++ im2txt_NIC/main.py
@@ -21,12 +21,12 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--epoches', default=20, type=int, help="epoches in training-stage", dest='epoches')
     parser.add_argument('--batch_size', default=50, type=int, help="batch_size in training-stage", dest='batch_size')
+    parser.add_argument('--num_hidden', default=256, type=int, help="the number of hidden unit", dest='num_hidden')
     parser.add_argument('--lr', default=0.01, type=float, help="learning rate in training-stage", dest='lr')
     parser.add_argument('--freq_val', default=5, type=int, help="frequence of validation", dest='freq_val')
-    parser.add_argument('--num_hidden', default=256, type=int, help="the number of hidden unit", dest='num_hidden')
     parser.add_argument('--num_embed', default=256, type=int, help="the number of embedding dimension", dest='num_embed')
     parser.add_argument('--num_lstm_layer', default=256, type=int, help="the number of hidden_unit", dest='num_lstm_layer')
-    parser.add_argument('--gpu', default=None, type=str, help="wether run on gpu device", dest='gpus')
+    parser.add_argument('--gpu', default=None, type=str, help="wether run on gpu device", dest='gpu')
     parser.add_argument('--prefix', default='./checkpoint/train', type=str, help="prefix of save checkpoint", dest='prefix')
     parser.add_argument('--period', default=5, type=int, help="times to save checkpoint in training-stage", dest='period')
     return parser.parse_args()
@@ -71,12 +71,12 @@ def main(args):
     }
     cnn_sym = vgg16_fc7('image_data')
     cnn_exec = cnn_sym.simple_bind(ctx=ctx, is_train=False, **cnn_shapes)
-    lstm = caption_module(num_lstm_layer=num_lstm_layer, seq_len=train_data.sent_length,
+    lstm = caption_module(num_lstm_layer=num_lstm_layer, seq_len=train_data.sent_length+2,
                           vocab_size=train_data.vocab_size, num_hidden=num_hidden, num_embed=num_embed, batch_size=batch_size)
     lstm_shapes = {
         'image_feature': (batch_size, 4096),
-        'word_data': (batch_size, train_data.sent_length),
-        'softmax_label': (batch_size, train_data.sent_length)
+        'word_data': (batch_size, train_data.sent_length+2),
+        'softmax_label': (batch_size, train_data.sent_length+2)
     }
 
     lstm_exec = lstm.simple_bind(
@@ -112,6 +112,7 @@ def main(args):
             lstm_exec.arg_dict['softmax_label'] = batch.label
 
             lstm_exec.forward(is_train=True)
+            print batch.label
             params.eval_metric.update(labels=batch.label,
                                       preds=lstm_exec.outputs)
             lstm_exec.backward()
